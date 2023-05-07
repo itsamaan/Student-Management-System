d5c6e2577f6030f4a8fa2cd2eaf35d5fa8bc34dc
itsamaan <99782520+itsamaan@users.noreply.github.com>
2023-05-07 10:46:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-Coming Soon in progress.........
+from tkinter import *
+from tkinter import messagebox
+from PIL import ImageTk
+
+def login():
+    if usernameEntry.get()=='' or passwordEntry.get()=='':
+        messagebox.showerror('Error','Fields cannot be empty')
+    elif usernameEntry.get()=='Amaan' and passwordEntry.get()=='1234':
+        messagebox.showinfo('Success','Welcome')
+        
+    else:
+        messagebox.showerror('Error','Please enter correct credentials')
+
+
+
+window=Tk()
+
+window.geometry('1280x700+0+0')
+
+window.resizable(False,False)
+
+backgroundImage=ImageTk.PhotoImage(file='bg.jpg')
+
+
+
+
+
+window.mainloop()
+
+
+
+Still in progress.........
